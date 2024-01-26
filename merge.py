c8eb806510bd80893e83d927c2c66fa35c00cfaa
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-01-26 01:33:31 -0300

    update

--- merge.py
+++ merge.py
@@ -27,7 +27,7 @@ def main():
     model1 = PeftModel.from_pretrained(llama1, args.m1).merge_and_unload()
     model2 = PeftModel.from_pretrained(llama2, args.m2).merge_and_unload()
 
-    merging_method = MergingMethod(merging_method_name="average_merging")
+    merging_method = MergingMethod(merging_method_name="mask_merging")
 
     merged_model = pretrained_model
     merged_model = merging_method.get_merged_model(merged_model=merged_model,
@@ -42,11 +42,11 @@ def main():
                                                    nums_regmean_examples=None,
                                                    reduce_non_diagonal_ratio=None,
                                                    param_value_mask_rate=None,
-                                                   weight_format=None,
-                                                   weight_mask_rates=None,
-                                                   use_weight_rescale=None,
-                                                   mask_strategy=None,
-                                                   mask_apply_method=None,
+                                                   weight_format="delta_weight",
+                                                   weight_mask_rates=[0.9,0.9],
+                                                   use_weight_rescale=True,
+                                                   mask_strategy="random",
+                                                   mask_apply_method="average_merging",
                                                    models_use_deepcopy=False)
 
 
