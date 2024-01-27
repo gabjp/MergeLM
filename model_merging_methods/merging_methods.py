075b98d3c48076e7f8f3739c599fe30c97425ba7
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-01-27 01:23:39 -0300

    update

--- model_merging_methods/merging_methods.py
+++ model_merging_methods/merging_methods.py
@@ -462,7 +462,9 @@ class MergingMethod:
             num_mask_params = int(flattened_models_to_merge_param.shape[1] * param_value_mask_rate)
             print("oi2", flush=True)
             # Tensor, shape (num_models_to_merge, 1), find the num_mask_params-th smallest magnitude element of all the parameters in each individual model
-            kth_values, _ = flattened_models_to_merge_param.abs().kthvalue(k=num_mask_params, dim=1, keepdim=True)
+            abs_val = flattened_models_to_merge_param.abs()
+            print("oi2.5", flush=True)
+            kth_values, _ = abs_val.kthvalue(k=num_mask_params, dim=1, keepdim=True)
             print("oi3", flush=True)
             # Tensor, shape (num_models_to_merge, num_total_params), where True is for parameters that we want to preserve
             mask = flattened_models_to_merge_param.abs() >= kth_values
