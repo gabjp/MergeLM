6af2bab43cf251461ec545e2342e02ea0a2bcdc2
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-02-08 19:59:06 -0300

    updat

--- inference_llms_instruct_math_code.py
+++ inference_llms_instruct_math_code.py
@@ -556,7 +556,7 @@ if __name__ == "__main__":
                         choices=["WizardLM-7B-V1.0", "WizardLM-13B-V1.2", "WizardLM-70B-V1.0",
                                  "WizardMath-7B-V1.0", "WizardMath-13B-V1.0", "WizardMath-70B-V1.0",
                                  "WizardCoder-Python-7B-V1.0", "WizardCoder-Python-13B-V1.0", "WizardCoder-Python-34B-V1.0",
-                                 "llama-2-13b-code-alpaca"])
+                                 "llama-2-13b-code-alpaca", 'merged'])
     parser.add_argument("--dataset_name", type=str, default="alpaca_eval", help="dataset to be used", choices=["alpaca_eval", "gsm8k", "MATH", "human_eval", "mbpp"])
     parser.add_argument("--start_index", type=int, default=0)
     parser.add_argument("--end_index", type=int, default=sys.maxsize)
