9a873f2cca6e5458196b14ccb2f8aadccd647683
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-02-08 22:17:55 -0300

    updat

--- inference_llms_instruct_math_code.py
+++ inference_llms_instruct_math_code.py
@@ -82,11 +82,14 @@ def recover_from_pretrained_model(finetuned_model_name, pretrained_model_name, a
 def create_llm(finetuned_model_name, pretrained_model_name, args, logger: logging.Logger, tensor_parallel_size=1, just_inference=False, save_model_path=None):
     if just_inference:
         if os.path.exists(os.path.join(cache_dir, finetuned_model_name)):
-            print(os.path.join(cache_dir, finetuned_model_name))
             llm = LLM(model=os.path.join(cache_dir, finetuned_model_name), tensor_parallel_size=tensor_parallel_size)
-        else:
-            assert os.path.exists(finetuned_model_name)
+        elif os.path.exists(finetuned_model_name):
             llm = LLM(model=finetuned_model_name, tensor_parallel_size=tensor_parallel_size)
+        else:
+            if 'Wizard' in finetuned_model_name:
+                llm = LLM(model="WizardLM/"+finetuned_model_name, tensor_parallel_size=tensor_parallel_size)
+            else:
+                llm = LLM(model="layoric" +finetuned_model_name, tensor_parallel_size=tensor_parallel_size)
         assert save_model_path is None
     else:
         try:
