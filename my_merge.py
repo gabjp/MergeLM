716186c7b89381fc517aeb9b5327459f3a2679dc
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-03-01 16:56:47 -0300

    update

--- my_merge.py
+++ my_merge.py
@@ -16,6 +16,7 @@ def main():
     parser.add_argument("--save-path", type=str, default="")
     parser.add_argument("--m1", type=str, default="")
     parser.add_argument("--m2", type=str, default="")
+    parser.add_argument("--m3", type=str, default="")
     parser.add_argument("--llama-path", type=str, default="meta-llama/Llama-2-7b-hf")
     parser.add_argument("--method", type=str, default="")
     parser.add_argument("--DARE-method", type=str, default="")
@@ -25,12 +26,19 @@ def main():
     print(args)
 
     base_tokenizer = AutoTokenizer.from_pretrained(args.llama_path, use_fast=False)
-    base_model = AutoModelForCausalLM.from_pretrained(args.llama_path, cache_dir="../models/")
+    base_model = AutoModelForCausalLM.from_pretrained(args.llama_path)
 
     tokenizer1 = AutoTokenizer.from_pretrained(args.m1, use_fast=False)
     tokenizer2 = AutoTokenizer.from_pretrained(args.m2, use_fast=False)
     model1 = AutoModelForCausalLM.from_pretrained(args.m1)
     model2 = AutoModelForCausalLM.from_pretrained(args.m2)
+
+    if args.m3 != "":
+        model3 = AutoModelForCausalLM.from_pretrained(args.m3)
+        tokenizer3 = AutoTokenizer.from_pretrained(args.m3, use_fast=False)
+
+    models_to_merge = [model1, model2] if args.m3 == "" else [model1, model2, model3]
+    tokenizers = [tokenizer1, tokenizer2] if args.m3 == "" else [tokenizer1, tokenizer2, tokenizer3]
 
     smart_tokenizer_and_embedding_resize(
         special_tokens_dict=dict(pad_token="[PAD]"),
@@ -38,36 +46,13 @@ def main():
         tokenizer=base_tokenizer
     )
 
-    print(model2.get_input_embeddings().weight.data.size())
-    print(model2.get_output_embeddings().weight.data.size())
-    print()
     
-    for finetuned_model, finetuned_tokenizer in zip([model1, model2], [tokenizer1, tokenizer2]):
-        print("!")
+    for finetuned_model, finetuned_tokenizer in zip(models_to_merge, tokenizers):
         smart_tokenizer_and_embedding_resize(
             special_tokens_dict=dict(pad_token="[PAD]"),
             model=finetuned_model,
             tokenizer=finetuned_tokenizer
         )
-
-    print(model1.get_input_embeddings().weight.data.size())
-    print(model1.get_output_embeddings().weight.data.size())
-
-
-    print(model2.get_input_embeddings().weight.data.size())
-    print(model2.get_output_embeddings().weight.data.size())
-    print()
-
-
-
-    print(base_model.get_input_embeddings().weight.data.size())
-    print(base_model.get_output_embeddings().weight.data.size())
-
-    for (n,m1),(_,m2),(_,b) in zip(model1.named_parameters(), model2.named_parameters(), base_model.named_parameters()):
-        if m1.size() != b.size():
-            print(f"m1 mismatch {n},{m1.size()},{b.size()}")
-        if m2.size() != b.size():
-            print(f"m2 mismatch {n},{m2.size()},{b.size()}")
 
 
     print('models loaded', flush=True)
@@ -76,7 +61,7 @@ def main():
     merging_method = MergingMethod(merging_method_name=args.method)
     merged_model = base_model
     merged_model = merging_method.get_merged_model(merged_model=merged_model,
-                                                models_to_merge=[model1, model2],
+                                                models_to_merge=models_to_merge,
                                                 exclude_param_names_regex=[],
                                                 trainers=[None, None],
                                                 scaling_coefficient=None,
@@ -94,13 +79,16 @@ def main():
                                                 mask_apply_method=args.DARE_method,
                                                 models_use_deepcopy=False)
 
-    for name in ["/merged_model", "/tokenizer_1", "/tokenizer_2"]:
+    for name in ["/merged_model", "/tokenizer_1", "/tokenizer_2", "tokenizer_3"]:
         if not os.path.exists(args.save_path + name):
             os.makedirs(args.save_path + name)
     
     merged_model.save_pretrained(args.save_path + "/merged_model")
     tokenizer1.save_pretrained(args.save_path + "/tokenizer_1")
     tokenizer2.save_pretrained(args.save_path + "/tokenizer_2")
+
+    if args.m3 != "":
+        tokenizer3.save_pretrained(args.save_path + "/tokenizer_3")
     
 if __name__ == "__main__":
     main()
